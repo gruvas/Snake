3cc005c9334497a73f7b78728918438bd797d2d3
gruvas <gruvas67@gmail.com>
2023-05-08 21:28:37 +0300

    Diagonal transition bug fixed

--- Snake/wwwroot/js/components/Snake/LastFieldCheck.js
+++ Snake/wwwroot/js/components/Snake/LastFieldCheck.js
@@ -9,12 +9,14 @@
 	lastField = lastField.match(/\d+/g)
 	lastField = Array.from(lastField[0])
 
-	if ((firstField[0] == lastField[0] || firstField[0] == parseInt(lastField[0]) - 1 || firstField[0] == parseInt(lastField[0]) + 1) &&
-		(firstField[1] == lastField[1] || firstField[1] == parseInt(lastField[1]) - 1 || firstField[1] == parseInt(lastField[1]) + 1)) {
-		return true
-	}
+	console.log(firstField, lastField)
 
-	return false
+	const dx = parseInt(lastField[0]) - firstField[0];
+	const dy = parseInt(lastField[1]) - firstField[1];
+	const isDiagonal = Math.abs(dx) === 1 && Math.abs(dy) === 1;
+	const isAdjacent = (dx === 0 || dy === 0) && !isDiagonal;
+
+	return isAdjacent;
 }
 
 export default lastFieldCheck
