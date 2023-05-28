6636bc48cedb28d8bc199f0d55393fdc4a6f179e
gruvas <gruvas67@gmail.com>
2023-05-28 14:09:27 +0300

    Bug fixed

--- Snake/wwwroot/js/components/Snake/LastFieldCheck.js
+++ Snake/wwwroot/js/components/Snake/LastFieldCheck.js
@@ -11,12 +11,20 @@
 
 	console.log(firstField, lastField)
 
-	const dx = parseInt(lastField[0]) - firstField[0];
-	const dy = parseInt(lastField[1]) - firstField[1];
-	const isDiagonal = Math.abs(dx) === 1 && Math.abs(dy) === 1;
-	const isAdjacent = (dx === 0 || dy === 0) && !isDiagonal;
+	const dx = parseInt(lastField[0]) - firstField[0]
+	const dy = parseInt(lastField[1]) - firstField[1]
+	const isDiagonal = Math.abs(dx) === 1 && Math.abs(dy) === 1
+	const isAdjacent = (dx === 0 || dy === 0) && !isDiagonal
 
-	return isAdjacent;
+	if ((firstField[0] == lastField[0] || firstField[0] == parseInt(lastField[0]) - 1 || firstField[0] == parseInt(lastField[0]) + 1) &&
+		(firstField[1] == lastField[1] || firstField[1] == parseInt(lastField[1]) - 1 || firstField[1] == parseInt(lastField[1]) + 1)) {
+		if (isAdjacent) {
+			return true
+
+		}
+	}
+
+	return false
 }
 
 export default lastFieldCheck
